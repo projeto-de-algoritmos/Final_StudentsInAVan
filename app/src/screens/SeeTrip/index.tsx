147a6e0990f9c9d900a5e8f6f3d198737cd56883
liversonp <liverson.p@gmail.com>
2022-09-15 20:58:57 -0300

    Correções no memoization

--- app/src/screens/SeeTrip/index.tsx
+++ app/src/screens/SeeTrip/index.tsx
@@ -11,30 +11,53 @@ export default function FindWayScreen({navigation}) {
   var capacity = 15;
   var n = qtdStudents.length;
   var result;
-  var memo = Array(15);
+  var memo = Array(16);
+  var catched = Array(16);
+  let valNotCatch = 0;
 
     function knapsack(n,capacity,qtdStudents){
-        if(n == 0 || capacity == 0){
+        //console.log("bag: ", capacity, "posArray: ", n);
+        //console.log("posicao no array: ",n);
+        //console.log("valor no array: ",qtdStudents[n]);
+        //console.log("não pegando: ",valNotCatch);
+        if(n == -1 || capacity == 0){
             return 0;
         }
         if(qtdStudents[n] > capacity){
-            memo[capacity-1][n] = memo[capacity-1][n-1];
-            return memo[capacity-1][n];
+            memo[capacity][n+1] = knapsack(n-1,capacity,qtdStudents);
+            return memo[capacity][n+1];
         }
-            let aux = capacity-qtdStudents[n];
-            memo[capacity-1][n] = Math.max(aux+knapsack(n-1, aux, qtdStudents), knapsack(n-1,capacity,qtdStudents));
-            return memo[capacity-1][n];
+
+        let aux = capacity-qtdStudents[n]
+        let valCatch = qtdStudents[n]+knapsack(n-1, aux, qtdStudents);
+        valNotCatch += knapsack(n-1,capacity,qtdStudents);
+        //console.log("pegando: ", valCatch)
+
+        if(valCatch > valNotCatch){
+            memo[capacity][n+1] = valCatch;
+            catched[capacity][n+1] = 1; 
+        }
+        else{
+            memo[capacity][n+1] = valNotCatch;
+            catched[capacity][n+1] = 0;
+        }
+        //console.log("rodei ate o final");
+        return memo[capacity][n+1];
     }
 
     function alertResult(){
+        console.log("quantidade de alunos: ", qtdStudents);
+        
         for(let i=0;i<memo.length;i++){
-            memo[i] = new Array(n).fill(-1);
+            memo[i] = new Array(n+1).fill(-1);
+            catched[i] = new Array(n+1).fill(0);
         }
+
         for(let i=0;i<memo.length;i++){
             memo[i][0] = 0;
         }
         knapsack(n-1, capacity, qtdStudents);
-        Alert.alert(memo[capacity-1][n-1].toString());
+        Alert.alert(memo[capacity][n-1].toString());
     }
 
     return (
